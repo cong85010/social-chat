f04a6d32afcad34e118aea2aee0182a7aa9cf91c
cong85010 <cong85010@gmail.com>
2022-09-16 00:02:26 -0700

    add redux + login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,18 +3,24 @@ import 'antd/dist/antd.min.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from '~/components/login/Login';
 import Chat from '~/components/chat/Chat';
+import { message } from 'antd';
 
 function App() {
- return (
-  <div className="App">
-   <Routes>
-    <Route path="/">
-     <Route index element={<Login />} />
-     <Route path="messenger" element={<Chat />} />
-    </Route>
-   </Routes>
-  </div>
- );
+    message.config({
+        right: 100,
+        duration: 2,
+        maxCount: 3,
+    });
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Login />} />
+                    <Route path="messenger" element={<Chat />} />
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- front-end/src/utils/color.js
+++ front-end/src/utils/color.js
@@ -1,5 +1,6 @@
+export const URL = 'http://54.254.219.40:5000';
+
 export const primaryColor = '#556b2f';
 export const bgColor = '#fff';
 export const border = '#a9a9a9';
 export const text = '#0168ff';
-
