bd7c71c4a70443074e802e70a6408f2896312fce
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2022-09-28 21:36:43 +0700

    check lai

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -33,8 +33,8 @@ function App() {
                     }
                 />
 
-                {/* <Route path="/login" element={<Login />} /> */}
-                <Route path="/login" element={<Chat />} />
+                <Route path="/login" element={<Login />} />
+                {/* <Route path="/login" element={<Chat />} /> */}
 
             </Routes>
         </div>
